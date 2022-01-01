5f870c7bbfa6cd4d71b4128e9f6f0f9321725e56
MrJanHorak <janny_b_goode@hotmail.com>
2021-12-31 21:40:14 -0500

    add names to projects and tweak css styling

--- src/Components/ContentCard/ContentCard.jsx
+++ src/Components/ContentCard/ContentCard.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
 const ContentCard = (props) => {
-console.log(props)
   return(
     props.work.map((element, index) =>
-    <div className='card'>
+    <div key={1+index} className='card'>
     <div className='card-info' key={index}>      
     {element}
     <br /><br />
-    <a href={props.links[index]}>Visit Site</a>
+    <a key={20+index} href={props.links[index]}>Visit Site</a>
     </div>
-    <div className='card-image'>
+    <div  key={30+index} className='card-image'>
+    <a href={props.links[index]}>
     <img className='card-image' src={props.projectPics[index]} alt="placeholder" />
+    </a>
     </div>
     </div>
   ))

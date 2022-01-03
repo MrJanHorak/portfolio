cc6394fe1ec7941266d716fd5e3a8561be7a6b30
MrJanHorak <janny_b_goode@hotmail.com>
2022-01-02 22:12:17 -0500

    correct typo in data js

--- src/assests/data.js
+++ src/assests/data.js
@@ -2,7 +2,7 @@ const work = [
   "Junction\n\nA Social networking app for people seeking others with similar interests and hobbies to meet up or share insight, things learned or accomplishments. Using React, JavaScript, MaterialUi.",
   "Sticky-Note-Bulletin-Board\n\nAn attempt to create a classic bulletin board in a web-browser. Using Django, Python, JavaScript and jQuery. Offers option to use a personal background, a personal board and team view board.",
   "Homecooked\n\nA social web-site for recipe sharing and 7-mealplan creation. Uses MongoDb and EJS implementing Google OAuth.",
-  "Battleship2021\n\nA Remake of the game Battleship using Vanilla JavaScript. One player places the boats, the second tries to guess where they are at. One twist in this version is that a spy gives away the location of one boat.",
+  "Battleship2022\n\nA Remake of the game Battleship using Vanilla JavaScript. One player places the boats, the second tries to guess where they are at. One twist in this version is that a spy gives away the location of one boat.",
   "Tic-Tac-Toe\n\nThe Classic game of Tic-Tac-Toe. This is written in Vanilla JavaScript in my early days of learning. I have gone back and added a computer player to the game simply using a series of conditional statements to mimic my own though process of how I play the game.",
 ];
 

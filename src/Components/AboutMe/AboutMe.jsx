8399f8c9cd31835e4d1552933c21164c485b0d17
Jan Horak <janny_b_goode@hotmail.com>
2022-01-30 11:58:19 -0500

    fix typo on aboutme page

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -10,12 +10,12 @@ const AboutMe = () => {
         <p>
           Multicultural, bilingual full stack developer with a background in
           creative communication. Using analytical approaches to solve
-          challenging problems brings a passion for collaboration, creativity
+          challenging problems bringing a passion for collaboration, creativity
           and compact solutions into each project. With a background in
           leadership and experience in working with people through life’s ups
           and downs, I approach each project with the understanding that team
           members share ownership and valuable input so that unique perspectives
-          that enrich the final outcome of a project.
+          enrich the final outcome of a project.
         </p>
       </div>
     </div>

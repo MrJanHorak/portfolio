8b7b33622a0d9cb4d0477b6a33fd86de8d68192c
Jan Horak <janny_b_goode@hotmail.com>
2022-05-17 13:49:18 -0400

    update skill icons to match b&w theme and add filter:invert to css

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,33 +1,10 @@
 import React, { useRef } from "react";
 
 const Landing = () => {
-  const storedTheme = localStorage.getItem("theme");
-  const prefersDark =
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches;
-  const prefersLight =
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: light)").matches;
-  let janHorakPortfolioLogo = useRef("");
-
-  if (storedTheme === "light" || (storedTheme === null && prefersLight)) {
-    janHorakPortfolioLogo = "./janHorakPortfolioLogo.png";
-  } else {
-    janHorakPortfolioLogo = "./janHorakPortfolioLogoInverse.png";
-  }
 
   return (
     <div className="landing">
       <picture>
-        <source srcSet={janHorakPortfolioLogo} />
-        <source
-          media="(prefers-color-scheme: light)"
-          srcSet="./janHorakPortfolioLogo.png"
-        />
-        <source
-          srcSet="./janHorakPortfolioLogoInverse.png"
-          media="(prefers-color-scheme: dark)"
-        />
         <img src="./janHorakPortfolioLogo.png" alt="logo" />
       </picture>
     </div>

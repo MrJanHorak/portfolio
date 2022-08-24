67efaf7a75c16d9abe0e2c59a69f7330a34266e4
Jan Horak <janny_b_goode@hotmail.com>
2022-08-23 23:32:37 -0400

    update passion projects to show word puzzle helper apps

--- src/assests/data.js
+++ src/assests/data.js
@@ -41,6 +41,7 @@ const projectPics = [
 
 const passionProjects = [
   "Spelling Bee Prep App\n\nThis passion project is the result of a conversation with my elementary age daughter who recently came home and said she wants to win her class's next year’s spelling bee. As a parent I do not always have the time to sit down and practice with her the way she may need it to reach her goal. So, I thought I should be able to do something about this. My goal is to create an app that will allow my daughter to practice as the app (device) reads the words out loud using speech synthesis and allows her to spell the word back.",
+  "3 in 1 Word Puzzle Solving Helpers\n\nThis set of three distinct projects grew out of the Letter Boxed Solver project, which was the first of these three tools. My 8th grade son was always finding better words to solve the New York times word puzzle than I, so I came up with an app to help me find possible solutions. After that I created the Spelling Bee helper, and finally a Wordle Solution helper. Each of these apps uses React, there is no backend and each has a unique algorithm to help find the solution. These were fun projects to practice CSS. After completing them all I wrapped them up in a nav bar linking to each site and gave each one a light and dark mode.",
   "FRY Word Diagnosis Tool\n\nThis app will start as a simple one to help track student acquisition of the first 300 Fry sight words.  Teachers for students in grades K-3 will be able to use the app to track the skill acquisition of individual students and print learning lists per student or for a group of students.  Words themselves will be categorized in order of frequency of occurrence in written text, as well as by regularity of spelling and included spelling patterns, so that teachers can choose word lists for instruction based on those useful characteristics.  Flashcards or word lists for groups or individual students may be printed for teacher reference. Later versions of the app may include research-supported instruction and games that include responsive feedback and track student progress for the teacher.",
   "Arduino Based weather station\n\nSeveral years ago I was part of a team that sent up a weather balloon to the stratosphere, some of the technology we used from that is still laying around my garage so I thought: I should be able to build something with some of this, and thus the idea of a weather station running off an Arduino or Raspberry Pi came to mind. It is a side project I hope to realize in the next few months.",
 
@@ -48,16 +49,19 @@ const passionProjects = [
 
 const passionProjectsLinks = [
   "https://spelling-bee-practice-app.netlify.app/",
+  "https://wordle-solving-helper.netlify.app/",
   "",
   ""]
 
 const passionProjectsRepositories = [
   "https://github.com/MrJanHorak/spelling-bee-practice-tool",
+  "https://github.com/MrJanHorak/wordle-solution-helper",
   "https://github.com/MrJanHorak/high-frequency-word-diagnosis-tool",
   ""]
 
 const passionProjectPics = [
   "./passionProjects/spellingbeeappanimated.gif",
+  "./passionProjects/word-puzzle-helpers.gif",
   "./passionProjects/sightWords.jpg",
   "./passionProjects/arduino.jpg",
 ];

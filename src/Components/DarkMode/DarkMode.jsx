2826bdccb6bd04f8281fb39aeee93f6babedf237
MrJanHorak <janny_b_goode@hotmail.com>
2022-10-22 22:29:52 -0400

    remove slider and create switching mode icon

--- src/Components/DarkMode/DarkMode.jsx
+++ src/Components/DarkMode/DarkMode.jsx
@@ -1,48 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { setPicturesThemed } from "../../assests/setPicturesThemed";
 import { FaGithub } from "react-icons/fa";
-
-const setDark = () => {
-  localStorage.setItem("theme", "dark");
-  document.documentElement.setAttribute("data-theme", "dark");
-};
-
-const setLight = () => {
-  localStorage.setItem("theme", "light");
-  document.documentElement.setAttribute("data-theme", "light");
-};
-
-const storedTheme = localStorage.getItem("theme");
-
-const prefersDark =
-  window.matchMedia &&
-  window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-const defaultDark =
-  storedTheme === "dark" || (storedTheme === null && prefersDark);
-
-if (defaultDark) {
-  setDark();
-  setPicturesThemed("dark");
-}
-
-const toggleTheme = (e) => {
-  if (e.target.checked) {
-    setDark();
-    setPicturesThemed("dark");
-  } else {
-    setLight();
-    setPicturesThemed("light");
-  }
-};
+import { CiLight } from "react-icons/ci";
+import { MdOutlineDarkMode } from "react-icons/md";
+import { IoColorPaletteSharp } from "react-icons/io5";
 
 const DarkMode = () => {
+  const [mode, setMode] = useState(localStorage.getItem("theme"));
+
+  const setDark = () => {
+    localStorage.setItem("theme", "dark");
+    document.documentElement.setAttribute("data-theme", "dark");
+  };
+
+  const setLight = () => {
+    localStorage.setItem("theme", "light");
+    document.documentElement.setAttribute("data-theme", "light");
+  };
+
+  const storedTheme = localStorage.getItem("theme");
+
+  const prefersDark =
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+  const defaultDark =
+    storedTheme === "dark" || (storedTheme === null && prefersDark);
+
+  let themeIcon = <CiLight />;
+
+  if (defaultDark) {
+    setDark();
+    setPicturesThemed("dark");
+    themeIcon = <MdOutlineDarkMode />;
+  }
+
+  const toggleTheme = (e) => {
+    if (storedTheme === "light") {
+      setDark();
+      setPicturesThemed("dark");
+      setMode("dark");
+      themeIcon = <MdOutlineDarkMode />;
+    } else {
+      setLight();
+      setPicturesThemed("light");
+      setMode("light");
+      themeIcon = <CiLight />;
+    }
+  };
+
+  // useEffect(() => {console.log(mode)}, [mode]);
+
   return (
     <div className="toggle-theme-wrapper">
+      <IoColorPaletteSharp />
       <span role="img" aria-label="light theme">
-        ⚪
+        <div onClick={toggleTheme}>{themeIcon}</div>
       </span>
-      <label className="toggle-theme" htmlFor="checkbox">
+      {/* <label className="toggle-theme" htmlFor="checkbox">
         <input
           type="checkbox"
           id="checkbox"
@@ -50,14 +65,16 @@ const DarkMode = () => {
           defaultChecked={defaultDark}
         />
         <div className="slider round"></div>
-      </label>
-      <span role="img" aria-label="dark theme">
-        ⚫
-      </span>
+      </label> */}
+      {/* <span role="img" aria-label="dark theme">
+        <MdOutlineDarkMode />
+      </span> */}
       <div className="footer-info">
         <p>
           ©2022 Jan Horak
-          <a href="https://github.com/MrJanHorak/portfolio"><FaGithub/></a>
+          <a href="https://github.com/MrJanHorak/portfolio">
+            <FaGithub />
+          </a>
         </p>
       </div>
     </div>

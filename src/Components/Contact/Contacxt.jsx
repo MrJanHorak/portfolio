c63e50e00ad1918e0be1b022e7ee3c221b3ea1a4
MrJanHorak <janny_b_goode@hotmail.com>
2022-10-24 23:21:39 -0400

    remove contact pngs add icons

--- src/Components/Contact/Contacxt.jsx
+++ src/Components/Contact/Contacxt.jsx
@@ -1,35 +1,10 @@
 import React, { useRef } from "react";
+import { MdEmail } from "react-icons/md";
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
+import { Icon } from "@iconify/react";
+import { IconContext } from "react-icons/lib";
 
 const Contact = ({ onTouchStart, onTouchMove, onTouchEnd }) => {
-  const storedTheme = localStorage.getItem("theme");
-  let email = useRef("./email.png");
-  let linkedIn = useRef("./linkedIn-logo.png");
-  let gitHub = useRef("./gitHubLogo.png");
-  let resume = useRef("./resumeIcon.png");
-  // const prefersDark =
-  //   window.matchMedia &&
-  //   window.matchMedia("(prefers-color-scheme: dark)").matches;
-  const prefersLight =
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: light)").matches;
-
-  if (storedTheme === "light" || (storedTheme === null && prefersLight)) {
-    email = "./email.png";
-    linkedIn = "./linkedIn-logo.png";
-    gitHub = "./gitHubLogo.png";
-    resume = "./resumeIcon.png";
-  } else if (storedTheme === "dark" ){
-    email = "./emailInvert.png";
-    linkedIn = "./linkedIn-logoInverse.png";
-    gitHub = "./gitHubLogoInverse.png";
-    resume = "./resumeIconInvert.png";
-  } else if (storedTheme === "color"){
-    email = "./email.png";
-    linkedIn = "./linkedIn-logo.png";
-    gitHub = "./gitHubLogo.png";
-    resume = "./resumeIcon.png";
-  }
-
   return (
     <div
       id="contact-container"
@@ -38,71 +13,28 @@ const Contact = ({ onTouchStart, onTouchMove, onTouchEnd }) => {
       onTouchEnd={onTouchEnd}
     >
       <div id="contact-logo-container">
-        <div className="contact">
-          <a href="mailto: jan-horak@gmx.de">
-            <picture>
-              <source srcSet={email} />
-              <source
-                media="(prefers-color-scheme: light)"
-                srcSet="./email.png"
-              />
-              <source
-                srcSet="./emailInvert.png"
-                media="(prefers-color-scheme: dark)"
-              />
-              <img src={email} alt="email" />
-            </picture>
-          </a>
-        </div>
+        <IconContext.Provider value={{ size: "115px" }}>
+          <div className="contact">
+            <a href="mailto: jan-horak@gmx.de">
+              <MdEmail size="100%" />
+            </a>
+          </div>
 
-        <div className="contact">
-          <a href="https://www.linkedin.com/in/jan-horak/">
-            <picture>
-              <source srcSet={linkedIn} />
-              <source
-                media="(prefers-color-scheme: light)"
-                srcSet="./linkedIn-logo.png"
-              />
-              <source
-                srcSet="./linkedIn-logoInverse.png"
-                media="(prefers-color-scheme: dark)"
-              />
-              <img src={linkedIn} alt="linkedIn-logo" />
-            </picture>
-          </a>
-        </div>
+          <div className="contact">
+            <a href="https://www.linkedin.com/in/jan-horak/">
+              <FaLinkedin />
+            </a>
+          </div>
 
-        <div className="contact">
-          <a href="https://github.com/MrJanHorak">
-            <picture>
-              <source srcSet={gitHub} />
-              <source
-                media="(prefers-color-scheme: light)"
-                srcSet="./gitHubLogo.png"
-              />
-              <source
-                srcSet="./gitHubLogoInverse.png"
-                media="(prefers-color-scheme: dark)"
-              />
-              <img src={gitHub} alt="Github logo" />
-            </picture>
-          </a>
-        </div>
-
+          <div className="contact">
+            <a href="https://github.com/MrJanHorak">
+              <FaGithubSquare />
+            </a>
+          </div>
+        </IconContext.Provider>
         <div className="contact">
           <a href="./Horak Resume.pdf">
-            <picture>
-              <source srcSet={resume} />
-              <source
-                media="(prefers-color-scheme: light)"
-                srcSet="./resumeIcon.png"
-              />
-              <source
-                srcSet="./resumeIconInvert.png"
-                media="(prefers-color-scheme: dark)"
-              />
-              <img src={resume} alt="resume" />
-            </picture>
+            <Icon icon="academicons:cv-square" width="100" />
           </a>
         </div>
       </div>

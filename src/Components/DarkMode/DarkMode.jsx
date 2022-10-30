c99545dd2a459cabb7025236b45334c18d08ff23
MrJanHorak <janny_b_goode@hotmail.com>
2022-10-30 18:18:02 -0400

    add color/b&w toggle

--- src/Components/DarkMode/DarkMode.jsx
+++ src/Components/DarkMode/DarkMode.jsx
@@ -4,6 +4,7 @@ import { FaGithub } from "react-icons/fa";
 import { CiLight } from "react-icons/ci";
 import { MdOutlineDarkMode } from "react-icons/md";
 import { IoColorPaletteSharp } from "react-icons/io5";
+import { VscColorMode } from "react-icons/vsc";
 
 const DarkMode = () => {
   const [mode, setMode] = useState(localStorage.getItem("theme"));
@@ -23,6 +24,11 @@ const DarkMode = () => {
     document.documentElement.setAttribute("data-theme", "color");
   };
 
+  const setColorDark = () => {
+    localStorage.setItem("theme", "colorDark");
+    document.documentElement.setAttribute("data-theme", "colorDark");
+  };
+
   const storedTheme = localStorage.getItem("theme");
 
   const prefersDark =
@@ -33,53 +39,83 @@ const DarkMode = () => {
     storedTheme === "dark" || (storedTheme === null && prefersDark);
 
   let themeIcon = <CiLight />;
+  let colorIcon = <IoColorPaletteSharp />;
 
   if (defaultDark) {
     setDark();
     setPicturesThemed("dark");
+    themeIcon = <CiLight />;
+  }
+
+  if (mode === "light") {
+    setLight();
+    setPicturesThemed("light");
+    colorIcon = <IoColorPaletteSharp />;
     themeIcon = <MdOutlineDarkMode />;
   }
+
   if (mode === "color") {
     setColor();
     setPicturesThemed("color");
+    colorIcon = <VscColorMode />;
+    themeIcon = <MdOutlineDarkMode />;
+  }
+
+  if (mode === "colorDark") {
+    setColorDark();
+    setPicturesThemed("colorDark");
+    colorIcon = <VscColorMode />;
+    themeIcon = <CiLight />;
   }
 
   const toggleTheme = (e) => {
-    if (
-      storedTheme === "light" ||
-      storedTheme === "color" ||
-      storedTheme === null
-    ) {
+    if (storedTheme === "light" || storedTheme === null) {
       setDark();
       setPicturesThemed("dark");
       setMode("dark");
-      themeIcon = <MdOutlineDarkMode />;
-    } else if (
-      storedTheme === "dark" ||
-      storedTheme === "color" ||
-      storedTheme === null
-    ) {
+    } else if (storedTheme === "dark") {
       setLight();
       setPicturesThemed("light");
       setMode("light");
-      themeIcon = <CiLight />;
-    }
-  };
-
-  const toggleColor = (e) => {
-    if (storedTheme === "light" || storedTheme === "dark") {
+    } else if (storedTheme === "color") {
+      setColorDark();
+      setPicturesThemed("color");
+      setMode("colorDark");
+    } else if (storedTheme === "colorDark") {
       setColor();
       setPicturesThemed("color");
       setMode("color");
     }
   };
 
+  const toggleColor = (e) => {
+    if (storedTheme === "light") {
+      setColor();
+      setPicturesThemed("color");
+      setMode("color");
+      colorIcon = <VscColorMode />;
+    } else if (storedTheme === "dark") {
+      setColorDark();
+      setPicturesThemed("color");
+      setMode("colorDark");
+      colorIcon = <VscColorMode />;
+    } else if (storedTheme === "color") {
+      setLight();
+      setPicturesThemed("light");
+      setMode("light");
+      colorIcon = <IoColorPaletteSharp />;
+    } else if (storedTheme === "colorDark") {
+      setDark();
+      setPicturesThemed("dark");
+      setMode("dark");
+      colorIcon = <IoColorPaletteSharp />;
+    }
+  };
+
   return (
     <div className="toggle-theme-wrapper">
       <span role="img" aria-label="light theme">
-        <div onClick={toggleColor}>
-          <IoColorPaletteSharp />
-        </div>
+        <div onClick={toggleColor}>{colorIcon}</div>
       </span>
       <span role="img" aria-label="light theme">
         <div onClick={toggleTheme}>{themeIcon}</div>

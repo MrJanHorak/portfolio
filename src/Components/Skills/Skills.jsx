83cf2e02c598f6c5fe368e21c0d7a58d028faded
MrJanHorak <janny_b_goode@hotmail.com>
2022-11-04 21:36:20 -0400

    replace skill pictures with icons

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,21 +1,51 @@
-import React from "react";
-import skills from "../../assests/skills";
+import React from 'react';
+import {
+  IoLogoJavascript,
+  IoLogoPython,
+  IoLogoCss3,
+  IoLogoReact,
+} from 'react-icons/io5';
+import {
+  SiTypescript,
+  SiJquery,
+  SiMongodb,
+  SiPostgresql,
+  SiMysql,
+  SiAmazons3,
+  SiUbuntu,
+} from 'react-icons/si';
+import { FaJava, FaNodeJs } from 'react-icons/fa';
+import { TiHtml5 } from 'react-icons/ti';
+import { SiExpress } from 'react-icons/si';
+import { IconContext } from 'react-icons/lib';
 
 const Skills = ({ onTouchStart, onTouchMove, onTouchEnd }) => {
   return (
     <div
-      id="skills-container"
+      id='skills-container'
       onTouchStart={onTouchStart}
       onTouchMove={onTouchMove}
       onTouchEnd={onTouchEnd}
     >
-      <h1 className="mobileHeaders">Skills</h1>
-      <div id="gallery-container">
-        {skills.map((img, index) => (
-          <div key={100 + index}>
-            <img key={index} src={img} alt={img} />
-          </div>
-        ))}
+      <h1 className='mobileHeaders'>Skills</h1>
+      <div id='gallery-container'>
+        <IconContext.Provider value={{ size: '5em' }}>
+          <IoLogoJavascript />
+          <SiTypescript size='75' />
+          <IoLogoPython />
+          <FaJava />
+          <TiHtml5 />
+          <IoLogoCss3 />
+          <IoLogoReact />
+          <FaNodeJs />
+          <SiExpress />
+          <SiJquery />
+          <SiMongodb />
+          <SiPostgresql />
+          <SiMysql />
+          <SiAmazons3 />
+          <SiUbuntu />
+        </IconContext.Provider>
       </div>
     </div>
   );

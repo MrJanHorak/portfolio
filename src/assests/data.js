a7285b483572ccb41608b2e059f7e3e918517057
MrJanHorak <janny_b_goode@hotmail.com>
2023-02-11 22:30:18 -0500

    update links to new deployments

--- src/assests/data.js
+++ src/assests/data.js
@@ -11,9 +11,9 @@ const work = [
 const links = [
   "https://bridge-climate-app.netlify.app/",
   "https://frontend-nine-tawny.vercel.app/",
-  "https://sticky-note-bulletin-board.herokuapp.com/",
+  "https://stickynotebullitenboard.fly.dev/",
   "https://home-cooked-meal-planner.herokuapp.com/",
-  "https://junction.herokuapp.com/",
+  "https://community-junction.fly.dev/",
   "https://battleship2022.netlify.app/",
   "https://tictactoe-namenottaken.netlify.app/",
 ];

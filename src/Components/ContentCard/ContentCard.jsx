0a983e16ae46cb442371972b74752442490cc54f
MrJanHorak <janny_b_goode@hotmail.com>
2024-09-20 22:10:18 -0400

    update skill cards

--- src/Components/ContentCard/ContentCard.jsx
+++ src/Components/ContentCard/ContentCard.jsx
@@ -1,29 +1,23 @@
 import React from "react";
 import { FaGithub, FaLink } from "react-icons/fa";
+import "./ContentCard.css"; // Import the CSS file
 
 const ContentCard = (props) => {
   return (
     <div className="slider-card">
       {props.work.map((element, index) => (
-        <div key={index} className="card">
+        <div key={index} className={`card ${index % 2 === 0 ? "card-left" : "card-right"}`}>
           <div className="card-image-wrapper">
-            {/* <div className="card-image"> */}
-              {/* <img
-                src={props.projectPics[index]}
-                alt="project"
-                className="project-image"
-              /> */}
-            {/* </div> */}
-             <div className="image-stack">
-             {props.projectPics[index].map((image, index) => (
-          <img 
-            key={index} 
-            src={image} 
-            alt={`${element.title} - ${index + 1}`} 
-            className="project-image"
-          />
-        ))}
-      </div>
+            <div className="image-stack">
+              {props.projectPics[index].map((image, imgIndex) => (
+                <img 
+                  key={imgIndex} 
+                  src={image} 
+                  alt={`${element.title} - ${imgIndex + 1}`} 
+                  className="project-image"
+                />
+              ))}
+            </div>
           </div>
           <div className="card-info">
             <h3>{element}</h3>
